a03187ec802389a7cb8755ddd6bae37e6c38999f
donnymays <dtmays2@gmail.com>
2020-10-02 08:43:08 -0700

    passing test for .find method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,6 +3,10 @@ require 'word'
 
 describe '#Word' do
   
+  before(:each) do
+    Word.clear()
+  end
+
   describe(".all") do
     it("is empty at first") do   
       expect(Word.all()).to(eq([]))
@@ -27,6 +31,14 @@ describe '#Word' do
     word2.save()
     Word.clear()
     expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it('finds specific word from its ID') do
+      word1 = Word.new({:name => "blue", :id => nil})
+      word1.save()
+      expect(Word.find(word1.id)).to(eq(word1))
     end
   end
 
