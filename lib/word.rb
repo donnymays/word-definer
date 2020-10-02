7f74a583073da673e8b610799a1064d68dc07591
donnymays <dtmays2@gmail.com>
2020-10-02 08:23:59 -0700

    fail for .all test

---
+++ lib/word.rb
@@ -0,0 +1,15 @@
+class Word
+  attr_reader :name, :id
+
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values
+  end
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,2 +1,12 @@
 require 'rspec'
-require ' word'
+require 'word'
+
+describe '#Word' do
+  
+  describe(".all") do
+    it("is empty at first") do
+      expect(Word.all()).to(eq([1]))
+    end
+  end 
+
+end
