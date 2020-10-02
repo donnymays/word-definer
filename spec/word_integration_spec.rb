10bd6a6bdee75b39ff91f2151cafc3e139d14d13
donnymays <dtmays2@gmail.com>
2020-10-02 09:12:48 -0700

    passing '/' integration spec

---
+++ spec/word_integration_spec.rb
@@ -0,0 +1,18 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe '#Word' do
+
+  before(:each) do
+    Word.clear()
+  end
+
+  describe('create an intial path', {:type => :feature}) do
+    it('creates a landing page') do
+      visit('/')
+      expect(page).to have_content('Word')
+    end
+  end
+end
