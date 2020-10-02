e757c18ef6d3be03d1007efdfe002387c986e75f
donnymays <dtmays2@gmail.com>
2020-10-02 08:31:14 -0700

    failed #save test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,9 +4,19 @@ require 'word'
 describe '#Word' do
   
   describe(".all") do
-    it("is empty at first") do
+    it("is empty at first") do   
       expect(Word.all()).to(eq([]))
     end
   end 
+
+  describe('#save') do
+    it("saves a word") do
+      word1 = Word.new({:name => "blue", :id => nil})
+      word1.save()
+      word2 = Word.new({:name => "green", :id => nil})
+      word2.save()
+      expect(Word.all).to(eq([word1, word2]))
+    end
+  end
 
 end
